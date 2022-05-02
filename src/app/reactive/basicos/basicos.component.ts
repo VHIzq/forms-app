4504ebf2acef991b314d517396e37e71772ef1b4
Victor Hugo <vrmq@gft.com>
2022-05-02 18:18:08 -0500

    add style design

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
+  /* Formulario se puede crear con el servicio FormBuilder */
+  /* miFormulario: FormGroup = new FormGroup({
+    'nombre': new FormControl('RTX 4080TI'),
+    'precio': new FormControl(1800),
+    'existencias': new FormControl( 4 ),
+  }); */
 
-  constructor() { }
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: [ 'RTX 4080Ti', [ Validators.required, Validators.minLength(3) ] ],
+    precio: [ 0, [ Validators.required, Validators.min(10) ] ],
+    existencias: [ 5, [Validators.required, Validators.min(0)] ],
+  })
+  constructor( private formBuilder: FormBuilder ) {
 
-  ngOnInit(): void {
   }
-
 }
