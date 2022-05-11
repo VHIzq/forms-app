7ec9fc1d06e45d2fe5e535eac3973d1f6376a21d
Victor Hugo <vrmq@gft.com>
2022-05-11 11:10:16 -0500

    deshaciendo cambio try2

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -7,47 +7,62 @@ import { EmailValidatorService } from '../../../shared/validator/email-validator
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegistroComponent implements OnInit {
-
-  miFormulario: FormGroup = this.fb.group( {
-    nombre: ['', [ Validators.required, Validators.pattern( this.validatorService.nombreApellidoPattern ) ]],
-    email: [ '', [ Validators.required, Validators.pattern( this.validatorService.emailPattern ) ], [ this.emailValidator ]],
-    username: [ '', [ Validators.required, this.validatorService.nicknameNoValido ]],
-    password: [ '', [ Validators.required, Validators.minLength(6) ]],
-    password2: [ '', [ Validators.required ]]
-  }, {
-    Validators: [ this.validatorService.camposIguales('password', 'password2') ]
-  })
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.nombreApellidoPattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorService.nicknameNoValido],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      Validators: [
+        this.validatorService.camposIguales('password', 'password2'),
+      ],
+    }
+  );
 
   constructor(
     private fb: FormBuilder,
     private validatorService: ValidatorService,
     private emailValidator: EmailValidatorService
-    ) {}
+  ) {}
 
   ngOnInit(): void {
-
-    this.miFormulario.reset( {
+    this.miFormulario.reset({
       nombre: 'Kaoru Tanamachi',
       email: 'test1@test.com',
-      username: 'TestUser'
-    })
+      username: 'TestUser',
+    });
   }
 
-  campoNoValido( campo: string ) {
+  campoNoValido( campo: string) {
     return this.miFormulario.get(campo)?.invalid
             && this.miFormulario.get(campo)?.touched;
   }
 
   submitFormulario() {
-    console.log(
-      this.miFormulario.value
-    );
+    console.log(this.miFormulario.value);
     this.miFormulario.markAllAsTouched;
   }
-
-
 }
+
